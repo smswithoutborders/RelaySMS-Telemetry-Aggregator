039cca29b20e9270fb3e654076b7e1cb582378bf
Vanessa Christopher <vanessaigwe1@gmail.com>
2025-01-15 14:45:23 +0100

    feat: add total_retained_users_with_tokens to summary

--- data_retriever.py
+++ data_retriever.py
@@ -46,6 +46,7 @@ def get_summary(params: dict):
         metrics_summary = {
             "total_signup_users": signup_metrics["total_signup_users"],
             "total_retained_users": retained_metrics["total_retained_users"],
+            "total_retained_users_with_tokens": retained_metrics["total_retained_users_with_tokens"],
             "total_signup_countries": signup_metrics["total_countries"],
             "total_retained_countries": retained_metrics["total_countries"],
             "signup_countries": signup_metrics["countries"],
